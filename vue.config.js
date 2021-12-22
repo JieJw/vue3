62ba2f83691541e3513bef003a35b28dfaf75303
JieJw <jiewang234@sfmail.sf-express.com>
2021-12-22 16:54:33 +0800

    add：初始化项目

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+function addStyleResource(rule) {
+  rule
+    .use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [path.resolve(__dirname, './src/assets/css/variable.less')],
+    })
+}
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@components': resolve('./src/components'),
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    const types = ['vue', 'vue-modules']
+    types.forEach((type) =>
+      addStyleResource(config.module.rule('less').oneOf(type))
+    )
+  },
+}
